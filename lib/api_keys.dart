e35462fdc924c37f054acb6cec00b415420f4732
Enkang Yuan <yuan_enkang@outlook.com>
2024-03-28 15:45:55 -0500

    updated .gitignore to exclude api_keys.dart

--- lib/api_keys.dart
+++
@@ -1,8 +0,0 @@
-// firebase api keys
-const firebaseWebKey = 'AIzaSyDiTBEDwm5exld7Szs0EunjCo0KlQPVj4E';
-const firebaseAndroidKey = 'AIzaSyAYK52PewzQbHi9_PQLYB_BgOFQFXswmhU';
-const firebaseIosKey = 'AIzaSyCimT0uMZv9xmHbaOfrKCWVWpCymxLDj8Q';
-const firebaseMacosKey = 'AIzaSyCimT0uMZv9xmHbaOfrKCWVWpCymxLDj8Q';
-
-// tomtom maps
-const tomtomApiKey = 'LC0XV2ujfvI8gSfZYeuGHRfn6Q60Km9H';
